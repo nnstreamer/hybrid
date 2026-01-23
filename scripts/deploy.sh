044675be362520a13b48a8693a346575d825db29
MyungJoo Ham <myungjoo.ham@samsung.com>
2026-01-23 14:20:56 +0900

    fix: heredoc escape fix
    Some variable are to be evaluated in AWS, not in github action.
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -167,26 +167,26 @@ if [[ -n "${COMPUTE_EIF_S3_URI}" && "${ALLOW_PREBUILT_EIF}" != "true" ]]; then
   exit 1
 fi
 
-TOKEN="$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" || true)"
-if [[ -n "${TOKEN}" ]]; then
-  COMPUTE_HOST="$(curl -s -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4 || true)"
+TOKEN="\$(curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" || true)"
+if [[ -n "\${TOKEN}" ]]; then
+  COMPUTE_HOST="\$(curl -s -H "X-aws-ec2-metadata-token: \${TOKEN}" http://169.254.169.254/latest/meta-data/local-ipv4 || true)"
 else
-  COMPUTE_HOST="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 || true)"
+  COMPUTE_HOST="\$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4 || true)"
 fi
-if [[ -z "${COMPUTE_HOST}" ]]; then
-  COMPUTE_HOST="$(hostname -I | awk '{print $1}' || true)"
+if [[ -z "\${COMPUTE_HOST}" ]]; then
+  COMPUTE_HOST="\$(hostname -I | awk '{print $1}' || true)"
 fi
-if [[ -z "${COMPUTE_HOST}" ]]; then
+if [[ -z "\${COMPUTE_HOST}" ]]; then
   echo "Failed to determine compute host IP." >&2
   exit 1
 fi
-echo "Using COMPUTE_HOST=${COMPUTE_HOST}"
-echo "Using ROUTER_ADDRESS=${ROUTER_ADDRESS}"
+echo "Using COMPUTE_HOST=\${COMPUTE_HOST}"
+echo "Using ROUTER_ADDRESS=\${ROUTER_ADDRESS}"
 
 if [[ -n "${COMPUTE_EIF_S3_URI}" ]]; then
   aws s3 cp "${COMPUTE_EIF_S3_URI}" "\${EIF_PATH}"
 else
-  CONFIG_DIR="$(mktemp -d)"
+  CONFIG_DIR="\$(mktemp -d)"
   cat > "\${CONFIG_DIR}/router_com.yaml" <<CONFIG_EOF
 http:
   port: "\${ROUTER_COM_PORT}"
